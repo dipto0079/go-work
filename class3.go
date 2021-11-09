fd4791749f70bd99f998fa176506ba7bc8e3dd15
dipto0079 <sudiptobiswas.coder@gmail.com>
2021-11-09 22:39:38 +0600

    class 3

--- class3.go
+++ class3.go
@@ -3,20 +3,28 @@ package main
 import "fmt"
 
 type Person struct {
-	Name   string
-	Age    int
-	weight float64
+	Name    string
+	Age     int
+	weight  float64
+	IsAdult bool
 }
 
 func New(name string, age int, weight float64) *Person {
+	isAdult := age > 18
 	return &Person{
-		Name:   name,
-		Age:    age,
-		weight: weight,
+		Name:    name,
+		Age:     age,
+		weight:  weight,
+		IsAdult: isAdult,
 	}
 }
 
 func main() {
-	p := New("sudipto", 26, 72)
-	fmt.Println(*p)
+	p := Person{Name: "Sudipto", Age: 26, weight: 72, IsAdult: true}
+	p2 := New("sudipto", 26, 72)
+	fmt.Println(p)
+	fmt.Println(p2)
+}
+func (p *Person) SetName(name string) {
+
 }
